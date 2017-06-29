d60dd8848dd9aaac474720057ab23d9640e743dd
Kevin See <Kevin@qcinc.org>
2017-06-29 11:57:57 -0700

    added 2 functions to query PTAGIS for metadata about MRR and interrogation sites.

---
+++ R/queryMRRSites.R
@@ -0,0 +1,64 @@
+#' @title PTAGIS MRR sites
+#'
+#' @description Query and download metadata for PTAGIS MRR sites
+#'
+#' @author Kevin See
+#'
+#' @param site PTAGIS site code. Default is \code{NULL} which will query all sites
+#'
+#' @source \url{http://www.ptagis.org}
+#'
+#' @import dplyr httr purrr
+#' @return NULL
+#' @examples queryMRRSites()
+
+queryMRRSites = function(site = NULL) {
+
+  # assign user agent to the GitHub repo for this package
+  ua = httr::user_agent('https://github.com/KevinSee/PITcleanr')
+
+  # compose url with query
+  url_req = 'http://api.ptagis.org/mrrsites'
+
+  # send query to PTAGIS
+  if(is.null(site)) web_req = httr::GET(url_req, ua)
+
+  if(!is.null(site)) web_req = httr::GET(paste0(url_req, '/', site),
+                                         ua)
+
+  # if any problems
+  httr::stop_for_status(web_req,
+                        task = 'query data from PTAGIS')
+
+  # parse the response
+  parsed = httr::content(web_req,
+                         'parsed')
+
+  if(!is.null(site)) parsed = list(parsed)
+
+  parsed = parsed %>%
+    purrr::map(.f = function(x) {
+      for(i in 1:length(x)) {
+        if(is.null(x[[i]])) {
+          # if(names(x)[i] %in% c('facId', 'lineId', 'polyId')) {
+          #   x[[i]] = vector('integer', 1)
+          #   x[[i]] = NA
+          # }
+          if(names(x)[[i]] %in% c('isMarkSiteOnly', 'pointLocation')) {
+            x[[i]] = vector('logical', 1)
+            x[[i]] = NA
+          }
+          else x[[i]] = vector('character', 1)
+        }
+      }
+      return(x)
+    }) %>%
+    purrr::map_df(`[`,
+                  c('siteCode', 'siteName', 'rkmMask', 'siteType', 'siteTypeDescription', 'huC4Code', 'latitude', 'longitude', 'coordinateType', 'minKm', 'maxKm', 'isMarkSiteOnly', 'changedBy', 'changeComments', 'changeDate', 'pointLocation', 'shapeType', 'locationId')) %>%
+    dplyr::mutate_at(vars(latitude:longitude,
+                          minKm:maxKm),
+                     funs(as.numeric))
+
+  return(parsed)
+
+}
