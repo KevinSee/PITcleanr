2ff203c5f58c7a5de4df4aedcdc405f93b562288
Kevin See <Kevin@qcinc.org>
2017-10-13 13:38:09 -0700

    Added function to pull out tag ID and trap date from LGR trap database

---
+++ R/summariseValidTagsLGR.R
@@ -0,0 +1,18 @@
+#' @title Summarise Valid Tags
+#'
+#' @description Filter the valid tag dataframe returned by \code{filterLGRtrapDB} to only include the tag code and the tagging date
+#'
+#' @author Kevin See
+#'
+#' @param valid_df dataframe of valid tags extracted from the trap database with the \code{filterLGRtrapDB} function
+#'
+#' @import dplyr
+#' @export
+#' @return NULL
+#' @examples summariseValidTagsLGR(filterLGRtrapDB(spawnYear = 2015))
+
+summariseValidTagsLGR = function(valid_df) {
+  valid_df %>%
+    group_by(TagID = LGDNumPIT) %>%
+    summarise(TrapDate = min(CollectionDate, na.rm = T))
+}
