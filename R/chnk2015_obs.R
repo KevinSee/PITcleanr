cc6bc71f7858cb2b363a5142e408cc0d7fb69441
Kevin See <Kevin@qcinc.org>
2017-10-13 13:36:15 -0700

    Added two example data tables

---
+++ R/chnk2015_obs.R
@@ -0,0 +1,9 @@
+#' @title Complete tag history for spawn year 2015 valid Chinook tags
+#'
+#' An example complete tag history observation file downloaded from PTAGIS after loading
+#' the valid tag ID returned from running the \code{filterLGRtrapDB} and \code{summariseValidTagsLGR} functions.
+#'
+#' @format A data frame with 120104 rows and 6 variables
+
+#' @source \url{http://www.PTAGIS.org/}
+"chnk2015_obs"
