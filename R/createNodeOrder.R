9dcfc10252840785f8cea117920bcc11cb524e5b
Kevin See <Kevin@qcinc.org>
2017-12-12 15:43:02 -0800

    Got rid of the need for parent-child table as input, and made the site_df input optional.

--- R/createNodeOrder.R
+++ R/createNodeOrder.R
@@ -8,9 +8,7 @@
 #'
 #' @param configuration a configuration file built by \code{buildConfig} function, and possibly modified by the user.
 #'
-#' @param parent_child is a data frame created by \code{createParentChildDf}.
-#'
-#' @param site_df a data frame created by one of the \code{write___NodeNetwork}.
+#' @param site_df Optional. A data frame created by one of the \code{write___NodeNetwork}.
 #'
 #' @param step_num in the \code{site_df} data frame, which step should be used to group various nodes? Default value is 1.
 #'
@@ -21,16 +19,21 @@
 
 createNodeOrder = function(valid_paths = NULL,
                            configuration = NULL,
-                           parent_child = NULL,
                            site_df = NULL,
                            step_num = 1) {
 
-  stopifnot(!is.null(valid_paths),
-            !is.null(parent_child),
-            !is.null(site_df))
+  stopifnot(!is.null(valid_paths))
 
   if(is.null(configuration)) configuration = buildConfig()
 
+  # get initial marking site
+  root_site = valid_paths %>%
+    filter(nchar(Path) == min(nchar(Path))) %>%
+    select(Node) %>%
+    as.matrix() %>%
+    as.character()
+
+  # construct node order, and add some RKM info
   node_order = valid_paths %>%
     split(list(.$Node)) %>%
     map_df(.id = 'EndNode',
@@ -38,65 +41,66 @@ createNodeOrder = function(valid_paths = NULL,
              tibble(Node = str_split(x$Path, ' ')[[1]])
 
            }) %>%
-    filter(Node %in% unique(parent_child$ChildNode[!is.na(parent_child$SiteType)])) %>%
     group_by(EndNode) %>%
-    mutate(NodeOrder = 1:n(),
-           PrevNode = lag(Node)) %>%
+    mutate(NodeOrder = 1:n()) %>%
     ungroup() %>%
     select(Node, NodeOrder) %>%
-    distinct(Node, .keep_all=TRUE) %>%
+    mutate(Node = ifelse(NodeOrder == 1 & Node == '',
+                         root_site,
+                         Node)) %>%
     full_join(valid_paths,
               by = c('Node')) %>%
-    filter(!is.na(Path)) %>%
+    distinct() %>%
+    filter(!(Node == root_site & NodeOrder > 1)) %>%
+    mutate(NodeSite = Node) %>%
+    mutate(NodeSite = ifelse(nchar(Node) > 3,
+                             stringr::str_replace(NodeSite, 'A0$', ''),
+                             NodeSite)) %>%
+    mutate(NodeSite = ifelse(nchar(Node) > 3,
+                             stringr::str_replace(NodeSite, 'B0$', ''),
+                             NodeSite)) %>%
     left_join(configuration %>%
-                select(SiteID, Node, matches('RKM')) %>%
-                rowwise() %>%
-                filter(grepl(SiteID, Node)) %>%
-                ungroup() %>%
-                distinct())
+                mutate(NodeSite = gsub('A0$', '',
+                                       gsub('B0$', '', Node)),
+                       NodeSite = ifelse(nchar(NodeSite) == 1 & grepl('A0$', SiteID),
+                                         SiteID,
+                                         NodeSite)) %>%
+                select(NodeSite, matches('RKM')) %>%
+                distinct() %>%
+                group_by(NodeSite) %>%
+                filter(RKMTotal == min(RKMTotal[RKMTotal > 0])) %>%
+                slice(1) %>%
+                ungroup())
 
-  init_brch = node_order %>%
-    filter(NodeOrder == 2) %>%
-    left_join(configuration %>%
-                select(SiteID,
-                       Node,
-                       RKM,
-                       RKMTotal) %>%
-                distinct()) %>%
-    group_by(Node) %>%
-    filter(RKMTotal == min(RKMTotal, na.rm = T)) %>%
-    ungroup() %>%
-    left_join(site_df %>%
-                select(SiteID,
-                       Group = paste0('Step', step_num)) %>%
-                mutate(Group = factor(Group,
-                                      levels = unique(Group)))) %>%
-    filter(!is.na(Group)) %>%
-    mutate(Group = forcats::fct_reorder(Group, RKMTotal, min)) %>%
-    arrange(Group, RKM)
+  if(!is.null(site_df)) {
+    init_brch = node_order %>%
+      filter(NodeOrder == 2) %>%
+      left_join(site_df %>%
+                  select(NodeSite = SiteID,
+                         Group = paste0('Step', step_num)) %>%
+                  mutate(Group = factor(Group,
+                                        levels = unique(Group)))) %>%
+      filter(!is.na(Group)) %>%
+      mutate(Group = forcats::fct_reorder(Group, RKMTotal, min)) %>%
+      arrange(Group, RKM)
 
-  init_brch = init_brch %>%
-    # filter(Group %in% Group[duplicated(Group)]) %>%
-    # group_by(Group) %>%
-    # slice(1) %>%
-    # ungroup() %>%
-    # bind_rows(anti_join(init_brch,
-    #                     .,
-    #                     by = 'Group')) %>%
-    # arrange(Group, RKM) %>%
-    mutate(BranchNum = 1:n())
+    init_brch = init_brch %>%
+      mutate(BranchNum = 1:n())
 
-  node_order$BranchNum = NA
-  for(i in 1:nrow(init_brch)) {
-    node_order$BranchNum[grep(init_brch$Node[i], node_order$Path)] = i
+    node_order$BranchNum = NA
+    for(i in 1:nrow(init_brch)) {
+      node_order$BranchNum[grep(init_brch$Node[i], node_order$Path)] = i
+    }
+
+    node_order = node_order %>%
+      left_join(init_brch %>%
+                  select(BranchNum,
+                         Group)) %>%
+      arrange(BranchNum, RKM) %>%
+      filter(!(is.na(BranchNum) & NodeOrder != 1))
+
   }
 
-  node_order = node_order %>%
-    left_join(init_brch %>%
-                select(BranchNum,
-                       Group)) %>%
-    arrange(BranchNum, RKM) %>%
-    filter(!(is.na(BranchNum) & NodeOrder != 1))
 
   return(node_order)
 }
