c49d82cb3d30f5e893c3db70a771a0ccfd66322d
Kevin See <Kevin@qcinc.org>
2019-02-06 14:41:35 -0800

    corrected typo, package "stringr" had been "stingr"

--- R/nodeEfficiency.R
+++ R/nodeEfficiency.R
@@ -16,7 +16,7 @@
 #'
 #' @examples nodeEfficiency()
 #'
-#' @import dplyr stingr purrr
+#' @import dplyr stringr purrr
 #' @export
 #' @return NULL
 nodeEfficiency <- function(capHist_proc, node_order, node = NULL, direction = c('upstream', 'downstream')){
