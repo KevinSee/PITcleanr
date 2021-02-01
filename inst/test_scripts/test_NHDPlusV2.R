e66eac7276eeed9b0e489fd964d95f8ce6f964ed
Kevin See <Kevin.See@merck.com>
2021-02-01 11:00:02 -0800

    wrote a script for testing the various NHDPlus queries, including plotting the sites and flowlines.

---
+++ inst/test_scripts/test_NHDPlusV2.R
@@ -0,0 +1,173 @@
+# Author: Kevin See
+# Purpose: Test functions related to downloading and using NHDPlus v2 layer
+# Created: 2/1/2021
+# Last Modified: 2/1/2021
+# Notes: some new functions are based on those written by Ryan Kinzer.
+
+#-----------------------------------------------------------------
+# load needed libraries
+library(tidyverse)
+library(sf)
+# library(QRFcapacity)
+library(nhdplusTools)
+library(magrittr)
+# library(PITcleanr)
+devtools::load_all()
+
+#-----------------------------------------------------------------
+# download all the metadata for all sites from PTAGIS
+all_meta = queryPtagisMeta()
+
+# which sites do we care about for this exercise?
+model_sites = writeLGRNodeNetwork()
+xtabs(~ Step3, model_sites)
+
+# model_sites = writeTUMNodeNetwork()
+# model_sites = writePRONodeNetwork()
+
+# create sf points of a subset of sites
+sub_sites = model_sites %>%
+  # filter(Step3 == "SFSalmon") %>%
+  # filter(SiteID == "GRA" | Step3 == "SFSalmon") %>%
+  filter(SiteID == "GRA" | Step2 == "NE_Oregon") %>%
+  select(SiteID) %>%
+  left_join(all_meta %>%
+              select(SiteID = siteCode,
+                     siteName,
+                     siteType,
+                     coordinateType,
+                     latitude,
+                     longitude,
+                     rkm) %>%
+              distinct()) %>%
+  filter(!is.na(latitude)) %>%
+  st_as_sf(coords = c("longitude",
+                      "latitude"),
+           crs = 4326) %>%
+  st_transform(5070)
+
+# which site is furthest downstream?
+root_site = sub_sites %>%
+  filter(SiteID == "UGR")
+
+# download the NHDPlus v2 flowlines
+# do you want flowlines downstream of root site? Set to TRUE if you have downstream sites
+dwn_flw = T
+nhd_list = queryFlowlines(sites_sf = sub_sites,
+                           root_site_code = root_site$SiteID,
+                           dwnstrm_sites = dwn_flw,
+                           dwn_min_stream_order = 1)
+
+
+flowlines = nhd_list$flowlines
+if(dwn_flw) {
+  flowlines %<>%
+  rbind(nhd_list$dwn_flowlines)
+}
+
+# join sites to nearest hydro sequence
+sites_NHDseg = st_join(sub_sites,
+                       flowlines %>%
+                         select(Hydroseq),
+                       join = st_nearest_feature)
+
+
+# plot the flowlines and the sites
+ggplot() +
+  geom_sf(data = flowlines,
+          aes(color = as.factor(StreamOrde))) +
+  scale_color_viridis_d(direction = -1,
+                        end = 0.7) +
+  geom_sf(data = nhd_list$basin,
+          fill = NA,
+          lwd = 2) +
+  geom_sf(data = sites_NHDseg,
+          size = 4,
+          color = "black") +
+  geom_sf_label(data = sub_sites,
+                aes(label = SiteID)) +
+  theme_bw() +
+  labs(color = "Stream\nOrder")
+
+
+
+#-----------------------------------------------------------------
+# Build parent-child tables
+# these are site-based, not node-based
+
+# Original parent child table, based on older functions
+# use this to compare new output
+# build an initial configuration file
+org_config = buildConfig()
+parent_child_df = createParentChildDf(model_sites %>%
+                                        filter(SiteID %in% sub_sites$SiteID),
+                                      org_config,
+                                      startDate = "20190101") %>%
+  mutate(across(c(ParentNode, ChildNode),
+                str_remove,
+                "A0$"),
+         across(c(ParentNode, ChildNode),
+                str_remove,
+                "B0$")) %>%
+  filter(ParentNode != ChildNode) %>%
+  rename(ParentSite = ParentNode,
+         ChildSite = ChildNode)
+
+# use new functions, and not the configuration file
+parent_child_test = sites_NHDseg$SiteID %>%
+  as.list() %>%
+  rlang::set_names() %>%
+  map_df(.id = "SiteID",
+         .f = function(x) {
+           dwn_site = try(findDwnstrmSite(x,
+                                          flow_lines = flowlines,
+                                          sites_joined = sites_NHDseg))
+           if(class(dwn_site) == "try-error") {
+             dwn_site = NA_character_
+           }
+           tibble(downSite = dwn_site) %>%
+             return()
+         }) %>%
+  select(ParentSite = downSite,
+         ChildSite = SiteID) %>%
+  # mutate(ParentSite = replace_na(ParentSite,
+  #                                "TUM")) %>%
+  left_join(all_meta %>%
+              select(ParentSite = siteCode,
+                     parent_rkm = rkm) %>%
+              distinct()) %>%
+  left_join(all_meta %>%
+              select(ChildSite = siteCode,
+                     child_rkm = rkm) %>%
+              distinct()) %>%
+  left_join(sites_NHDseg %>%
+              st_drop_geometry() %>%
+              select(ChildSite = SiteID,
+                     child_hydro = Hydroseq)) %>%
+  filter(!(is.na(ParentSite) &
+             ChildSite == root_site$SiteID)) %>%
+  arrange(child_hydro)
+# arrange(parent_rkm,
+#         child_rkm)
+
+# compare with PITcleanr parent-child table
+parent_child_test
+parent_child_df
+
+anti_join(parent_child_test,
+          parent_child_df)
+
+anti_join(parent_child_df,
+          parent_child_test)
+
+
+parent_child_test %>%
+  rename(ParentNode = ParentSite,
+         ChildNode = ChildSite) %>%
+  getValidPaths(root_site = root_site$SiteID)
+
+findDwnstrmSite("LAKEC",
+                flowlines,
+                sites_NHDseg)
+
+
