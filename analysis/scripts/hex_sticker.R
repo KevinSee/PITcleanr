d4df0917b22106afe3aa9fd7405fe3370f33995a
Kevin See <Kevin.See@merck.com>
2021-02-03 12:30:44 -0800

    added a number of logo choices for an R hex sticker. updated the script to use some of these

--- analysis/scripts/hex_sticker.R
+++ analysis/scripts/hex_sticker.R
@@ -10,22 +10,69 @@
 library(hexSticker)
 
 #-----------------------------------------------------------------
+# Biomark colors
+# Primary
+# dark blue
+"#282D46"
+# light blue
+"#879FC2"
+# black
+"#000000"
+# gray
+"#A6A6A6"
+
+# Secondary
+# orange
+"#F28031"
+# green
+"#A0B63A"
+# dark blue
+"#00677F"
+# yellow
+"#F3D03E"
+
+# Tertiary
+# orange
+"#ECA154"
+"#EFBE7D"
+"#EFD19F"
+# green
+"#C4D600"
+"#CEDC00"
+"#E2E868"
+# blue
+"#0092BC"
+"#00A9CE"
+"#6AD1E3"
+# yellow
+"#F3D54E"
+"#F3DD6D"
+
+
+#-----------------------------------------------------------------
 # image to use
-img_path = "analysis/figures/broom.png"
+# img_path = "analysis/figures/broom.png"
+# img_path = "analysis/figures/PITtag_icon_yellow_bold.png"
+img_path = "analysis/figures/PITtag_icon_orange_bold.png"
+# img_path = "analysis/figures/PITtag_icon_green.png"
+
+# nm_color = "#F28031"
+nm_color = "#F3D03E"
 
 # create a logo sticker and save it
 s = sticker(magick::image_read(img_path),
-            package = "PITcleanr",
-            p_size = 9,
-            p_family = "sans",
             s_x = 1,
-            s_y = .75,
+            s_y = 0.7,
             s_width = 1,
-            s_height = 1,
-            # spotlight = T,
-            # h_fill = "#9e0b0f",
-            h_fill = "#63065f",
-            h_color = "#fec619",
+            s_height = 0.9,
+            package = "PITcleanr",
+            p_size = 8,
+            p_family = "sans",
+            # p_fontface = "bold",
+            p_color = nm_color,
+            # p_y = 0.6,
+            h_fill = "#282D46",
+            h_color = nm_color,
             filename = "analysis/figures/PITcleanr.png")
 
 plot(s)
