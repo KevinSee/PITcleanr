38c147d876351bc42a4d4fb70ed7d08f044a6240
Kevin See <Kevin.See@merck.com>
2021-02-03 17:01:52 -0800

    added function to compress PIT tag observations from PTAGIS

---
+++ R/compress.R
@@ -0,0 +1,101 @@
+#' @title Compress PTAGIS Data
+#'
+#' @description The function takes complete capture histories downloaded from PTAGIS
+#' and compresses them. This involves assigning nodes to the detection locations (e.g.
+#' sites, arrays, antennas, etc.), and summarizing the first and last detection time on
+#' each node as well as how many detections were made during that slot. Slots can be
+#' defined to include a maximum number of minutes, or not.
+#'
+#' Currently this function only works for PIT tag detections coded as `Observation`
+#' in PTAGIS. Other detection types are filtered out.
+#'
+#' @author Kevin See
+#'
+#' @param ptagis_file is the path to the PTAGIS observation file downloaded as a csv from PTAGIS.
+#' This must be the output from a complete capture history query.
+#' @param max_minutes maximum number of minutes between detections of a tag before it's considered a
+#' different "slot" of detections. Default is 60.
+#' @param configuration is a data frame which assigns node names to unique SiteID, AntennaID, and
+#' site configuration ID combinations. One example can be built with the function `buildConfig`. If
+#' no configuration file is provided, nodes are considered sites by default.
+#'
+#' @inheritParams base::difftime
+#' @import dplyr lubridate
+#' @importFrom janitor clean_names
+#' @importFrom readr read_csv
+#' @importFrom magrittr %<>%
+#' @export
+#' @return a tibble
+#' @examples compress()
+
+compress = function(ptagis_file = NULL,
+                    max_minutes = 60,
+                    configuration = NULL,
+                    units = c("mins",
+                              "auto", "secs", "hours",
+                              "days", "weeks")) {
+
+  stopifnot(!is.null(ptagis_file))
+
+  units = match.arg(units)
+
+  observations = suppressMessages(read_csv(ptagis_file)) %>%
+    janitor::clean_names() %>%
+    mutate(across(c(event_date_time_value,
+                    event_release_date_time_value),
+                  lubridate::mdy_hms)) %>%
+    filter(event_type_name == "Observation") %>%
+    arrange(tag_code, event_date_time_value)
+
+  if(!is.null(configuration)) {
+    observations %<>%
+      left_join(configuration %>%
+                  select(event_site_code_value = SiteID,
+                         antenna_group_configuration_value = ConfigID,
+                         antenna_id = AntennaID,
+                         node = Node),
+                by = c("event_site_code_value",
+                       "antenna_group_configuration_value",
+                       "antenna_id")) %>%
+      mutate(node = if_else(is.na(node),
+                            event_site_code_value,
+                            node))
+  } else {
+    observations %<>%
+      mutate(node = event_site_code_value)
+  }
+
+  slot_df = observations %>%
+    group_by(tag_code) %>%
+    mutate(prev_node = lag(node),
+           prev_time = lag(event_date_time_value),
+           time_diff = as.numeric(difftime(event_date_time_value, prev_time, units = 'mins')),
+           new_slot = if_else(node != prev_node | is.na(prev_node) | time_diff > max_minutes,
+                             T, F)) %>%
+    filter(new_slot) %>%
+    mutate(slot = 1:n()) %>%
+    select(tag_code,
+           event_type_name,
+           event_date_time_value,
+           node,
+           event_site_code_value,
+           antenna_id,
+           new_slot,
+           slot)
+
+  compress_summ = observations %>%
+    left_join(slot_df,
+              by = c("tag_code", "event_type_name", "event_date_time_value", "event_site_code_value", "antenna_id", "node")) %>%
+    tidyr::fill(slot, .direction = "down") %>%
+    group_by(tag_code,
+             node,
+             slot) %>%
+    summarise(n_dets = n(),
+              min_det = min(event_date_time_value),
+              max_det = max(event_date_time_value),
+              .groups = "drop") %>%
+    arrange(tag_code, slot) %>%
+    mutate(duration = difftime(max_det, min_det, units = units))
+
+  return(compress_summ)
+}
