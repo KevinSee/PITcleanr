013b38587a19602232fc2b361cc98d4ca417bca9
Kevin See <Kevin.See@merck.com>
2021-02-10 09:39:07 -1000

    added some other possibilities of inputs to these functions to ensure they work.

--- R/compress.R
+++ R/compress.R
@@ -49,9 +49,11 @@ compress = function(ptagis_file = NULL,
       mutate(across(c(event_date_time_value,
                       event_release_date_time_value),
                     lubridate::mdy_hms))
-  } else if(class(ptagis_file)[1] %in% c("tbl_df", "data.frame")) {
+  } else if(class(ptagis_file)[1] %in% c("spec_tbl_df", "tbl_df", "tbl", "data.frame")) {
     observations = ptagis_file %>%
       as_tibble()
+  } else {
+    stop("Trouble reading in ptagis_file.\n")
   }
 
   # perform some QC checks
