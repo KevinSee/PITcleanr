a8c4fb0ca28970bb5e7d67d86efe34ed9558fcb3
Kevin See <Kevin.See@merck.com>
2021-02-10 09:40:02 -1000

    added a function to build the node order, based on a parent child table. Meant to replace "createNodeOrder"

---
+++ R/buildNodeOrder.R
@@ -0,0 +1,34 @@
+#' @title Build Node Order
+#'
+#' @description Creates a data frame showing the path (through various detection sites) to
+#' reach each detection location in a parent-child table, and the node order of that location
+#'
+#' @author Kevin See
+#'
+#' @inheritParams buildPaths
+#'
+#' @import dplyr
+#' @export
+#' @return data frame containing a column,`end_loc`, showing the possible final locations,
+#' a column, `node_order`, showing the order of that node along the path,
+#' and a column, `path`, showing all the detection locations to pass on the way
+#' to that final location.
+#' @examples buildNodeOrder()
+
+buildNodeOrder = function(parent_child = NULL) {
+
+  paths_df = buildPaths(parent_child)
+
+  node_order = paths_df %>%
+    mutate(node_order = map_dbl(path,
+                                str_count,
+                                pattern = " "),
+           node_order = node_order + 1) %>%
+    rename(node = end_loc) %>%
+    select(node,
+           node_order,
+           path)
+
+  return(node_order)
+
+}
