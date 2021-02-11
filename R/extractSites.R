52b8407b0be3cc6133bf99480758e0dea055132f
Kevin See <Kevin.See@merck.com>
2021-02-10 14:49:42 -1000

    wrote function to extract all the sites found in an observation file, and attach some PTAGIS metadata to it.

---
+++ R/extractSites.R
@@ -0,0 +1,95 @@
+#' @title Extract Sites
+#'
+#' @description The function takes complete capture histories downloaded from PTAGIS
+#' and extracts all the detection sites from them. It matches those sites with
+#' information from PTAGIS metadata, being sure to pull the metadata from the
+#' correct configuration file.
+#'
+#' Currently this function only works for PIT tag detections coded as `Observation`
+#' in PTAGIS. Other detection types are filtered out.
+#'
+#' @author Kevin See
+#'
+#' @inheritParams compress
+#' @param as_sf Should the results be returned as an `sf` object (`TRUE`), or a tibble
+#' (`FALSE`, and the default)?
+#' @param if `as_sf = TRUE`, what CRS should the resulting `sf` object be
+#' transformed to? Default is 5070.
+#'
+#' @import dplyr lubridate
+#' @importFrom janitor clean_names
+#' @importFrom readr read_csv
+#' @importFrom magrittr %<>%
+#' @export
+#' @return a tibble
+#' @examples compress()
+
+extractSites = function(ptagis_file = NULL,
+                        as_sf = F,
+                        crs = 5070) {
+
+  stopifnot(!is.null(ptagis_file))
+
+  # get PTAGIS metadata
+  all_meta = queryPtagisMeta()
+
+  # read in observations
+  if(class(ptagis_file)[1] == "character") {
+    observations = suppressMessages(read_csv(ptagis_file)) %>%
+      janitor::clean_names() %>%
+      mutate(across(c(event_date_time_value,
+                      event_release_date_time_value),
+                    lubridate::mdy_hms))
+  } else if(class(ptagis_file)[1] %in% c("spec_tbl_df", "tbl_df", "tbl", "data.frame")) {
+    observations = ptagis_file %>%
+      as_tibble()
+  } else {
+    stop("Trouble reading in ptagis_file.\n")
+  }
+
+  # pull out all observation sites, and attach some metadata
+  obs_site = observations %>%
+    select(site_code = event_site_code_value,
+           antenna_id,
+           antenna_group_configuration_value) %>%
+    distinct() %>%
+    left_join(all_meta %>%
+                select(site_code = siteCode,
+                       site_name = siteName,
+                       site_type = siteType,
+                       type = Type,
+                       antenna_group_configuration_value = configurationSequence,
+                       antenna_id = antennaID,
+                       antenna_group_name = antennaGroupName,
+                       latitude,
+                       longitude,
+                       rkm,
+                       site_description = siteDescription)) %>%
+    select(-antenna_id,
+           -antenna_group_configuration_value,
+           -antenna_group_name) %>%
+    distinct()
+
+  # return as data frame or sf object
+  if(!as_sf) {
+    return(obs_site)
+  } else {
+
+    filter(obs_site,
+           is.na(latitude)) %>%
+      pull(site_code) %>%
+      paste(collapse = ',') %>%
+      paste("These sites don't have a latitude, and are excluded from results:\n",
+            ., "\n") %>%
+      cat()
+
+    obs_site %>%
+      filter(!is.na(latitude)) %>%
+      st_as_sf(coords = c("longitude",
+                          "latitude"),
+               crs = 4326) %>%
+      st_transform(crs = crs) %>%
+      return()
+  }
+
+}
