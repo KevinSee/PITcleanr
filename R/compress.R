c408e348bcbfcabc4093d8762ed7bbbeaed82712
Kevin See <Kevin.See@merck.com>
2021-04-05 10:27:02 -0700

    added a function readCTH to read ptagis data, and incorporated that function into compress and qcTagHistory functions.

--- R/compress.R
+++ R/compress.R
@@ -8,11 +8,7 @@
 #'
 #' @author Kevin See
 #'
-#' @param ptagis_file is the path to the PTAGIS observation file downloaded as a csv from PTAGIS.
-#' This must be the output from a Complete Tag History query (part of the Advanced Reporting).
-#' This query should contain: Tag, Mark Species, Mark Rear Type, Event Type, Event Site Type,
-#' Event Site Code, Event Date Time, Antenna, Antenna Group Configuration,
-#' Event Release Site Code, and Event Release Date Time.
+#' @inheritParams readCTH
 #' @param max_minutes maximum number of minutes between detections of a tag before it's considered a
 #' different "slot" of detections. Default is `NA`, which means a new slot is only defined by
 #' detections on a new node.
@@ -46,40 +42,11 @@ compress = function(ptagis_file = NULL,
 
   units = match.arg(units)
 
-  if(class(ptagis_file)[1] == "character") {
-    observations = suppressMessages(read_csv(ptagis_file)) %>%
-      janitor::clean_names()
-
-    # determine format of event date time value
-    n_colons = observations %>%
-      mutate(event_time = str_split(event_date_time_value, " ", simplify = T)[,2],
-             n_colon = str_count(event_time, "\\:")) %>%
-      pull(n_colon) %>%
-      max()
-
-    if(n_colons == 2) {
-      observations %<>%
-        mutate(across(c(event_date_time_value,
-                        event_release_date_time_value),
-                      lubridate::mdy_hms))
-    } else if(n_colons == 1) {
-      observations %<>%
-        mutate(across(c(event_date_time_value,
-                        event_release_date_time_value),
-                      lubridate::mdy_hm))
-    } else {
-      warning("Event Date Time Value has strange format.")
-    }
-
-  } else if(class(ptagis_file)[1] %in% c("spec_tbl_df", "tbl_df", "tbl", "data.frame")) {
-    observations = ptagis_file %>%
-      as_tibble()
-  } else {
-    stop("Trouble reading in ptagis_file.\n")
-  }
+  observations = readCTH(ptagis_file)
 
   # perform some QC checks
-  qc_list = qcTagHistory(observations)
+  qc_list = qcTagHistory(observations,
+                         ignore_event_vs_release = ignore_event_vs_release)
 
   if(!ignore_event_vs_release) {
   # identify batches of fish where lots of replicated event times or release times
