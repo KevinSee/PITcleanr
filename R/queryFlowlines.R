a750c3bd51b7e0331fb5ad4ae662b8c51e9e9e5c
Kevin See <Kevin.See@dfw.wa.gov>
2024-11-21 15:18:49 -0800

    updated queryFlowlines() function to make more efficient use of nhdplusTools package

--- R/queryFlowlines.R
+++ R/queryFlowlines.R
@@ -7,22 +7,20 @@
 #' @param sites_sf An `sf` class object containing points of all detection sites. Must contain a column named `site_code` containing the site code of each site.
 #' @param root_site_code Site code for the starting detection site.
 #' @param min_strm_order minimum stream order to query flowlines for. Default value is `0`.
-#' @param dwnstream_sites Does `sites_sf` contain sites that are downstream of `root_site_code`? If `TRUE`, then all flowlines within a boundary box of `sites_df` will be downloaded
-#' @param dwn_min_stream_order_diff Used to control the minimum stream order of downloaded downstream flowlines. This can be useful if the user wants to prevent downloading excessive smaller streams downstream of a tagging or release location. It corresponds to the difference between the stream order of the `root_site_code` site and the minimum stream order desired in the downstream flowlines. For example, if the `root_site_code` is located on a stream order 6 and `dwn_min_stream_order_diff = 2`, then only flowlines of at least 6-2=**4** will be downloaded. The default value is 0, meaning only streams of equal or greater order than the tagging or release site will be returned.
-#' @param buffer_dist when querying flowlines downstream of `root_site_code`, how big a buffer should be used around all sites in `sites_sf` to capture all the flowlines? Default is `0`. See `?sf::st_buffer()` for more help with possible formats.
+#' @param max_upstream_comid if provided, this function will filter out any flowlines upstream of this unique identifier (COMID) in the NHDPlus stream network. Default value is `NULL`. For assistance identifying a COMID, please see the `nhdplusTools` package, specifically the `discover_nhdplus_id()` function.
+#' @param combine_up_down Should the flowlines upstream and downstream of the starting point (`root_site_code`) be combined into a single object, or be returned as separate elements? The default value is `TRUE`
 #'
-#' @import dplyr sf nhdplusTools
-#' @importFrom magrittr %<>%
+#' @import dplyr sf nhdplusTools measurements tidyr purrr
 #' @importFrom nngeo st_remove_holes
 #' @export
-#' @return sf
+#' @return list with at least two elements: `flowlines` is the `sf` object of flowlines downloaded from NHDPlus. `basin` is the polygon representing the catchment upstream of the root site. If `combine_up_down` is `FALSE`, there is a third element in the list, called `dwn_flowlines`, containing an `sf` object of flowlines downstream of the root site.
 
 queryFlowlines = function(sites_sf = NULL,
                           root_site_code = NULL,
                           min_strm_order = 0,
-                          dwnstrm_sites = FALSE,
-                          dwn_min_stream_order_diff = NULL,
-                          buffer_dist = 0) {
+                          max_upstream_comid = NULL,
+                          combine_up_down = TRUE)
+{
 
   requireNamespace("nhdplusTools", quietly = TRUE)
 
@@ -33,122 +31,242 @@ queryFlowlines = function(sites_sf = NULL,
     root_site_code = sites_sf$site_code[1]
   }
 
-  # set minimum stream order for downstream flowlines
-  if(is.null(dwn_min_stream_order_diff)) dwn_min_stream_order_diff = 0
-
   # find the starting point (most downstream point)
   start_comid = sites_sf %>%
     dplyr::filter(site_code == root_site_code) %>%
-    sf::st_transform(crs = 4326) %>%
     nhdplusTools::discover_nhdplus_id()
 
-  # query flowlines from NHDPlus layer
-  cat(paste("Querying streams upstream of", root_site_code, "\n"))
-  nhd_lst = nhdplusTools::plot_nhdplus(outlets = list(start_comid),
-                                       streamorder = min_strm_order,
-                                       actually_plot = F)
+  start_pt <-
+    list(featureSource = "comid",
+         featureID = start_comid)
 
-  flowlines = nhd_lst$flowline %>%
-    sf::st_zm() %>%
-    sf::st_transform(crs = sf::st_crs(sites_sf))
+  # query flowlines from NHDPlus layer
+  message(paste("Querying streams upstream of", root_site_code, "\n"))
+
+  basin <-
+    nhdplusTools::get_nldi_basin(start_pt) |>
+    nngeo::st_remove_holes() |>
+    sf::st_transform(sf::st_crs(sites_sf))
+
+  if(!is.null(max_upstream_comid)) {
+
+    message("Removing some upstream areas")
+
+    upstrm_basin <-
+      nhdplusTools::get_nldi_basin(list(featureSource = "comid",
+                                        featureID = upstrm_comid)) |>
+      nngeo::st_remove_holes() |>
+      sf::st_transform(sf::st_crs(sites_sf))
+
+    new_basin <-
+      sf::st_difference(basin,
+                        upstrm_basin |>
+                          st_buffer(1000))
+    basin <-
+      new_basin
+
+    rm(new_basin)
+  }
+
+  flowlines <-
+    nhdplusTools::get_nhdplus(AOI = basin,
+                              realization = "flowline",
+                              streamorder = min_strm_order) |>
+    dplyr::mutate(
+      dplyr::across(
+        c(lakefract,
+          surfarea,
+          rareahload,
+          hwnodesqkm),
+        ~ as.numeric(.)))
+
 
   # a list to return
   # includes flowlines and polygon of basin
   return_list = list(flowlines = flowlines,
-                     basin = nngeo::st_remove_holes(nhd_lst$basin) %>%
-                       sf::st_zm() %>%
-                       sf::st_transform(crs = sf::st_crs(sites_sf)))
+                     basin = basin)
 
-  if(dwnstrm_sites) {
-    cat("Calculating furthest mainstem point downstream \n")
-    # get flowlines for downstream sites based on bounding box of all sites
-    if(as.numeric(buffer_dist) == 0) {
-     site_bb <-
-       sites_sf %>%
-       sf::st_transform(crs = 3857) %>%
-       sf::st_bbox()
-    } else {
-      site_bb <-
-        sites_sf %>%
-        sf::st_transform(crs = 3857) %>%
-        sf::st_buffer(dist = buffer_dist) %>%
-        sf::st_bbox()
-    }
-    nhd_lst_tmp <-
-      site_bb %>%
-      nhdplusTools::plot_nhdplus(bbox = .,
-                                 streamorder = max(min_strm_order, (max(flowlines$StreamOrde) - dwn_min_stream_order_diff)),
-                                 actually_plot = F)
+  # pull out sites downstream of root site
+  dwnstrm_sites_comid <-
+    sites_sf |>
+    dplyr::mutate(dwnstrm = purrr::map_lgl(geometry,
+                                           .f = function(x) {
+                                             !sf::st_covers(basin,
+                                                            x,
+                                                            sparse = F)
+                                           })) |>
+    dplyr::filter(dwnstrm |
+                    site_code == root_site_code) |>
+    dplyr::select(-dwnstrm)
+
+  if(nrow(dwnstrm_sites_comid) > 1 ) {
+    message("Calculating furthest mainstem point downstream \n")
+
+    # find COMID for all downstream sites
+    dwnstrm_sites_comid <-
+      dwnstrm_sites_comid |>
+      dplyr::rowwise() |>
+      dplyr::mutate(comid = nhdplusTools::discover_nhdplus_id(geometry)) |>
+      dplyr::ungroup()
+
+    # how far away are downstream sites from the root site?
+    dwnstrm_dist <-
+      sites_sf |>
+      dplyr::filter(site_code == root_site_code) |>
+      sf::st_distance(dwnstrm_sites_comid)
+
+    # maximum distance
+    dwnstrm_max <-
+      dwnstrm_dist[which.max(dwnstrm_dist)] |>
+      measurements::conv_unit(from = "m", to = "km")
+
+    message(paste("Querying streams downstream of", root_site_code, "\n"))
+
+    # query downstream mainstem starting from each downstream site
+    dwnstrm_main_fl <-
+      dwnstrm_sites_comid |>
+      tidyr::nest(data = -site_code) |>
+      dplyr::mutate(network = purrr::map2(data,
+                                          site_code,
+                                          .f = function(x,y) {
+                                            nhdplusTools::navigate_network(x,
+                                                                           mode = "DM",
+                                                                           output = "flowlines",
+                                                                           distance_km = dwnstrm_max) |>
+                                              suppressMessages() |>
+                                              sf::st_transform(st_crs(sites_sf)) |>
+                                              # dplyr::mutate(site_code = y) |>
+                                              # dplyr::select(site_code,
+                                              #               comid,
+                                              #               streamorde,
+                                              #               totdasqkm,
+                                              #               contains("hydroseq"),
+                                              #               geometry)
+                                              dplyr::mutate(
+                                                dplyr::across(
+                                                  c(lakefract,
+                                                    surfarea,
+                                                    rareahload,
+                                                    hwnodesqkm),
+                                                  ~ as.numeric(.))) |>
+                                              tibble::add_column(site_code = y,
+                                                                 .before = 0)
+                                          })) |>
+      dplyr::pull(network) |>
+      dplyr::bind_rows()
 
     # pull out the comid for the largest stream order and furthest downstream segment
-    dwn_comid = nhd_lst_tmp$flowline %>%
-      dplyr::filter(StreamOrde == max(StreamOrde)) %>%
-      # filter(Hydroseq == min(Hydroseq)) %>%
-      dplyr::filter(TotDASqKM == max(TotDASqKM)) %>%
-      dplyr::pull(COMID)
+    max_dwnstrm_comid <-
+      dwnstrm_main_fl |>
+      st_drop_geometry() |>
+      group_by(site_code) |>
+      filter(streamorde == max(streamorde)) |>
+      filter(totdasqkm == min(totdasqkm)) |>
+      ungroup() |>
+      filter(totdasqkm == max(totdasqkm)) |>
+      pull(comid) |>
+      unique()
 
-    # # determine stream order of hydrosequences of downstream sites
-    # min_strm_order <-
-    #   sites_sf |>
-    #   st_transform(st_crs(flowlines)) |>
-    #   st_join(flowlines |>
-    #             st_buffer(dist = 100) |>
-    #             select(starts_with("gnis"),
-    #                    StreamOrde,
-    #                    contains("Hydroseq")),
-    #           join = st_covered_by) |>
-    #   filter(!is.na(gnis_id)) |>
-    #   pull(StreamOrde) |>
-    #   max()
-
-
-    cat(paste("Querying streams downstream of", root_site_code, "\n"))
-    # nhd_lst_dwn = nhdplusTools::plot_nhdplus(outlets = list(dwn_comid),
-    #                                          # streamorder = max(flowlines$StreamOrde),
-    #                                          # streamorder = max(min_strm_order, (max(flowlines$StreamOrde) - dwn_min_stream_order_diff)),
-    #                                          streamorder = min_strm_order,
-    #                                          actually_plot = F)
-
-    nhd_lst_dwn = nhd_lst_tmp
-
-    # dwn_flowlines = nhd_lst_dwn$flowline %>%
-    #   dplyr::filter(!COMID %in% flowlines$COMID) %>%
-    #   sf::st_zm() %>%
-    #   sf::st_transform(crs = sf::st_crs(sites_sf))
-
-    dwn_site_comids = nhd_lst_dwn$flowline %>%
-      sf::st_zm() %>%
-      sf::st_transform(crs = sf::st_crs(sites_sf)) %>%
-      dplyr::select(COMID, Hydroseq) %>%
-      sf::st_join(sites_sf,
-              .,
-              join = st_nearest_feature) %>%
-      dplyr::filter(! COMID %in% flowlines$COMID) %>%
-      dplyr::as_tibble() %>%
-      dplyr::pull(COMID)
-
-    dwn_comids = c(start_comid,
-                   dwn_site_comids) %>%
-      as.list() %>%
-      map_df(.f = function(x) {
-        tibble(comid = nhdplusTools::get_DM(network = nhd_lst_dwn$flowline,
-                                            comid = x))
-      }) %>%
-      distinct() %>%
+    rm_comids <-
+      nhdplusTools::navigate_network(list(featureSource = "comid",
+                                          featureID = max_dwnstrm_comid),
+                                     mode = "DM",
+                                     output = "flowlines",
+                                     distance_km = dwnstrm_max) |>
+      suppressMessages() |>
+      st_drop_geometry() |>
+      filter(comid != max_dwnstrm_comid) |>
       pull(comid)
 
 
-    # dwn_comids = get_DM(nhd_lst_dwn$flowline,
-    #                     start_comid)
+    dwnstrm_main_fl <-
+      dwnstrm_main_fl |>
+      filter(!comid %in% rm_comids)
 
-    dwn_flowlines = nhd_lst_dwn$flowline %>%
-      dplyr::filter(COMID %in% dwn_comids) %>%
-      sf::st_zm() %>%
-      sf::st_transform(crs = sf::st_crs(sites_sf))
+    connected <-
+      if_else(dwnstrm_main_fl |>
+                filter(!dnhydroseq %in% unique(hydroseq),
+                       !comid %in% dwnstrm_sites_comid$comid) |>
+                nrow() == 0,
+              T, F)
 
-    return_list$dwn_flowlines = dwn_flowlines
+    if(connected) {
+      dwnstrm_flowline <-
+        dwnstrm_main_fl |>
+        select(-site_code) |>
+        distinct()
+    } else {
+
+      mainstem_lngth <-
+        dwnstrm_main_fl |>
+        select(-site_code) |>
+        distinct() |>
+        pull(lengthkm) |>
+        sum()
+
+      main_flowline <-
+        nhdplusTools::navigate_network(list(featureSource = "comid",
+                                            featureID = max_dwnstrm_comid),
+                                       mode = "UM",
+                                       output = "flowlines",
+                                       distance_km = mainstem_lngth) |>
+        suppressMessages() |>
+        st_transform(st_crs(sites_sf))
+
+      min_dwnstrm_comid <-
+        dwnstrm_main_fl |>
+        st_drop_geometry() |>
+        group_by(site_code) |>
+        filter(streamorde == max(streamorde)) |>
+        filter(totdasqkm == min(totdasqkm)) |>
+        ungroup() |>
+        filter(totdasqkm == min(totdasqkm)) |>
+        pull(comid) |>
+        unique()
+
+      rm_upstrm_comids <-
+        nhdplusTools::navigate_network(list(featureSource = "comid",
+                                            featureID = min_dwnstrm_comid),
+                                       mode = "UM",
+                                       output = "flowlines",
+                                       distance_km = mainstem_lngth) |>
+        suppressMessages() |>
+        st_drop_geometry() |>
+        filter(comid != min_dwnstrm_comid) |>
+        pull(comid) |>
+        unique()
+
+      main_flowline <-
+        main_flowline |>
+        filter(!comid %in% rm_upstrm_comids)
+
+
+      dwnstrm_flowline <-
+        dwnstrm_main_fl |>
+        select(-site_code) |>
+        distinct() |>
+        bind_rows(main_flowline |>
+                    dplyr::mutate(
+                      dplyr::across(
+                        c(lakefract,
+                          surfarea,
+                          rareahload,
+                          hwnodesqkm),
+                        ~ as.numeric(.)))) |>
+        distinct()
+
+    }
+
+    # should the flowlines upstream and downstream of the root site be combined?
+    if(combine_up_down) {
+      return_list$flowlines <-
+        return_list$flowlines |>
+        dplyr::bind_rows(dwnstrm_flowline) |>
+        distinct()
+    } else {
+      return_list$dwn_flowlines = dwnstrm_flowline
+    }
   }
 
-  # return(flowlines)
   return(return_list)
 }
